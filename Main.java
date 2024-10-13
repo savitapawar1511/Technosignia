c7931a4ae65d328b2383ae6805db64fb6013e710
savita <savitapawar1511@gmail.com>
2024-10-13 14:13:36 +0530

    add initial commit

---
+++ Main.java
@@ -0,0 +1,13 @@
+import java.util.Date;
+
+public class Main{
+
+
+    public static void main(String args[]){
+
+        System.out.println("Hello java");
+         System.out.println("Hello Technosignia");
+          System.out.println("Hello Jenkins");
+
+    }
+}
